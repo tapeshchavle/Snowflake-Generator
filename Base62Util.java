bea44221492340b346506a572f8d218e7d52a146
Tapesh Chavle <145046947+tapeshchavle@users.noreply.github.com>
2026-03-22 02:44:15 +0530

    Create Base62Util.java

---
+++ Base62Util.java
@@ -0,0 +1,16 @@
+public class Base62Util {
+
+    private static final String BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public static String encode(long value) {
+        StringBuilder sb = new StringBuilder();
+
+        while (value > 0) {
+            int remainder = (int) (value % 62);
+            sb.append(BASE62.charAt(remainder));
+            value /= 62;
+        }
+
+        return sb.reverse().toString();
+    }
+}
